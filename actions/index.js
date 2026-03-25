2b9eeec23a8022e7f2eb7bdcb1ccce97b0efddcb
Rohan-Saxena644 <rohsax83@gmail.com>
2026-03-25 19:51:02 +0530

    Using shadcn

---
+++ actions/index.js
@@ -0,0 +1,39 @@
+"use server";
+
+import dbConnect from "@/lib/dbConnect";
+import Contact from "@/models/Contact";
+
+
+export async function createContact(formData) {
+    try{
+        await dbConnect();
+        const { name, email, message } = formData;
+
+        if(!name || !email || !message) {
+            return {
+                success: false,
+                message: "All fields are required."
+            }
+        }   
+
+        const contact = await Contact.create({
+             name: name.trim(), 
+             email: email.trim().toLowerCase(), 
+             message: message.trim(), 
+        });
+
+        await contact.save();
+
+        return {
+            success: true,
+            message: "Your message has been submitted successfully!",
+            contactId: contact._id.toString()
+        }
+    }catch(error) {
+        console.error("Error creating contact:", error);
+        return {
+            success: false,
+            message: "An error occurred while submitting your message. Please try again later."
+        }
+    }
+}
--- components/contact-form.jsx
+++ components/contact-form.jsx
@@ -1,9 +1,49 @@
 'use client'
-import React from 'react'
+import React,{useState} from 'react'
+import {Button} from './ui/button'
+import {Input} from './ui/input'
+import {Textarea} from './ui/textarea'
+import { Label } from './ui/label'
+import {Card,CardContent,CardDescription,CardFooter,CardHeader,CardTitle} from './ui/card'
 
 const ContactForm = () => {
+
+    const [isSubmitting,setIsSubmitting] = useState(false);
+    const [message,setMessage] = useState("")
+
+
+    async function onSubmit(formData){
+        
+    }
+
   return (
-    <div>ContactForm</div>
+    <Card className="w-full max-w-2xl mx-auto">
+        <CardHeader>
+            <CardTitle>Contact Us</CardTitle>
+            <CardDescription>We would love to hear from you! Please fill out the form below to get in touch with us.</CardDescription>
+        </CardHeader>
+        <CardContent>
+            <form id="contact-form" onSubmit={onSubmit}>
+            <div className="grid w-full items-center gap-4">
+                <div className="grid w-full items-center gap-2">
+                    <Label htmlFor="name">Name</Label>
+                    <Input id="name" placeholder="Your Name" />
+                </div>
+                <div className="grid w-full items-center gap-2">
+                    <Label htmlFor="email">Email</Label>
+                    <Input id="email" type="email" placeholder="email"/>
+                </div>
+                <div className="grid w-full items-center gap-2">
+                    <Label htmlFor="message">Message</Label>
+                    <Textarea id="message" placeholder="Your Message" />
+                </div>
+            </div>
+            </form>
+        </CardContent>
+        <CardFooter>
+            <Button type="submit" disabled={isSubmitting} className="w-full">{isSubmitting ? "isSubmitting" : "Send Message"}</Button>
+        </CardFooter>
+    </Card>
   )
 }
 
